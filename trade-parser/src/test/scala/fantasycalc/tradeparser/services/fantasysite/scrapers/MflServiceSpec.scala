f87d337842c3f0fe607a885cb836bdf48f115e6b
Josh Cordell <joshua.k.cordell@gmail.com>
2021-09-15 22:23:21 -0500

    Simplify mocking of mfl client in test

--- trade-parser/src/test/scala/fantasycalc/tradeparser/services/fantasysite/scrapers/MflServiceSpec.scala
+++ trade-parser/src/test/scala/fantasycalc/tradeparser/services/fantasysite/scrapers/MflServiceSpec.scala
@@ -24,22 +24,8 @@ import org.scalatest.matchers.should.Matchers
 class MflServiceSpec extends AnyFunSpec with Matchers {
   describe("getLeagues") {
     it("should parse leagueId from response") {
-      val mockMflClient = new MflClient[Id] {
-        override def searchLeagues(
-          search: String
-        ): Id[LeagueSearchApiResponse] = ApiResponses.Mfl.SearchLeaguesResponse
-
-        override def getPlayers: Id[PlayersApiResponse] = ???
-
-        override def getTrades(leagueId: LeagueId): Id[TradesApiResponse] = ???
-
-        override def getRules(leagueId: LeagueId): Id[RulesApiResponse] = ???
-
-        override def getLeague(leagueId: LeagueId): Id[LeagueApiResponse] = ???
-      }
-
       val mflService: MflService[Id] =
-        new MflService[Id](mockMflClient, new PlayerIdConverterMock)
+        new MflService[Id](MockMflClient, new PlayerIdConverterMock)
       val actual: Id[List[LeagueId]] = mflService.getLeagues
       actual.head shouldBe LeagueId("10005")
       actual.length shouldBe 4
@@ -48,23 +34,8 @@ class MflServiceSpec extends AnyFunSpec with Matchers {
 
   describe("getTrades") {
     it("should parse trades and FantasycalcAssetId from transaction list") {
-      val mockMflClient = new MflClient[Id] {
-        override def searchLeagues(
-          search: String
-        ): Id[LeagueSearchApiResponse] = ???
-
-        override def getPlayers: Id[PlayersApiResponse] = ???
-
-        override def getTrades(leagueId: LeagueId): Id[TradesApiResponse] =
-          ApiResponses.Mfl.TradesResponse
-
-        override def getRules(leagueId: LeagueId): Id[RulesApiResponse] = ???
-
-        override def getLeague(leagueId: LeagueId): Id[LeagueApiResponse] = ???
-      }
-
       val mflService: MflService[Id] =
-        new MflService[Id](mockMflClient, new PlayerIdConverterMock)
+        new MflService[Id](MockMflClient, new PlayerIdConverterMock)
       val leagueId = LeagueId(123.toString)
       val actual: Id[List[Trade]] = mflService.getTrades(leagueId)
 
@@ -81,24 +52,8 @@ class MflServiceSpec extends AnyFunSpec with Matchers {
   describe("getSettings") {
     // TODO: Number of starter rules and PPR parsing isn't working yet
     it("should parse league settings") {
-      val mockMflClient = new MflClient[Id] {
-        override def searchLeagues(
-          search: String
-        ): Id[LeagueSearchApiResponse] = ???
-
-        override def getPlayers: Id[PlayersApiResponse] = ???
-
-        override def getTrades(leagueId: LeagueId): Id[TradesApiResponse] = ???
-
-        override def getRules(leagueId: LeagueId): Id[RulesApiResponse] =
-          ApiResponses.Mfl.RulesResponse
-
-        override def getLeague(leagueId: LeagueId): Id[LeagueApiResponse] =
-          ApiResponses.Mfl.LeagueResponse
-      }
-
       val mflService =
-        new MflService[Id](mockMflClient, new PlayerIdConverterMock)
+        new MflService[Id](MockMflClient, new PlayerIdConverterMock)
 
       val leagueId = LeagueId(123.toString)
       val actual = mflService.getSettings(leagueId)
@@ -112,4 +67,16 @@ class MflServiceSpec extends AnyFunSpec with Matchers {
       )
     }
   }
+}
+
+object MockMflClient extends MflClient[Id] {
+  override def searchLeagues(search: String): Id[LeagueSearchApiResponse] = ApiResponses.Mfl.SearchLeaguesResponse
+
+  override def getPlayers: Id[PlayersApiResponse] = ApiResponses.Mfl.PlayersResponse
+
+  override def getTrades(leagueId: LeagueId): Id[TradesApiResponse] = ApiResponses.Mfl.TradesResponse
+
+  override def getRules(leagueId: LeagueId): Id[RulesApiResponse] = ApiResponses.Mfl.RulesResponse
+
+  override def getLeague(leagueId: LeagueId): Id[LeagueApiResponse] = ApiResponses.Mfl.LeagueResponse
 }
