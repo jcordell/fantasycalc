d8803096f3a65f21e0aeaf32ef1e2cfdcaf92cda
Josh Cordell <joshua.k.cordell@gmail.com>
2021-09-19 23:40:00 -0500

    Move project to directory home

--- src/main/scala/fantasycalc/tradeparser/clients/MFlClient.scala
+++ src/main/scala/fantasycalc/tradeparser/clients/MFlClient.scala
@@ -3,6 +3,7 @@ package fantasycalc.tradeparser.clients
 import cats.effect.Concurrent
 import fantasycalc.tradeparser.models.ImplicitEntityCodecs._
 import fantasycalc.tradeparser.models.LeagueId
+import fantasycalc.tradeparser.models.api.mfl.MflPositionRules._
 import fantasycalc.tradeparser.models.api.mfl._
 import io.circe.generic.auto._
 import org.http4s.client.Client
@@ -22,21 +23,22 @@ trait MflClient[F[_]] {
 class MflClientImpl[F[_]: Concurrent](httpClient: Client[F])
     extends MflClient[F] {
   private val YEAR = 2021
+  private val API_TOKEN = ""
   private val BASE_URL = s"https://api.myfantasyleague.com/$YEAR"
 
   def searchLeagues(search: String): F[LeagueSearchApiResponse] = {
-    val url = s"$BASE_URL/export?TYPE=leagueSearch&SEARCH=$search&JSON=1"
+    val url = s"$BASE_URL/export?TYPE=leagueSearch&SEARCH=$search&JSON=1&APIKEY=$API_TOKEN"
     httpClient.expect[LeagueSearchApiResponse](url)
   }
 
   def getPlayers: F[PlayersApiResponse] = {
-    val url = s"$BASE_URL/export?TYPE=players&SEARCH&JSON=1"
+    val url = s"$BASE_URL/export?TYPE=players&SEARCH&JSON=1&APIKEY=$API_TOKEN"
     httpClient.expect[PlayersApiResponse](url)
   }
 
   def getTrades(leagueId: LeagueId): F[TradesApiResponse] = {
     val url =
-      s"$BASE_URL/export?TYPE=transactions&L=${leagueId.id}&APIKEY=&W=&TRANS_TYPE=TRADE&FRANCHISE=&DAYS=&COUNT=&JSON=1"
+      s"$BASE_URL/export?TYPE=transactions&L=${leagueId.id}&APIKEY=$API_TOKEN&W=&TRANS_TYPE=TRADE&FRANCHISE=&DAYS=&COUNT=&JSON=1"
     httpClient.expect[TradesApiResponse](url)
   }
 
@@ -48,7 +50,7 @@ class MflClientImpl[F[_]: Concurrent](httpClient: Client[F])
 
   def getRules(leagueId: LeagueId): F[RulesApiResponse] = {
     val url =
-      s"$BASE_URL/export?TYPE=rules&L=${leagueId.id}&JSON=1"
+      s"$BASE_URL/export?TYPE=rules&L=${leagueId.id}&JSON=1&APIKEY=$API_TOKEN"
     httpClient.expect[RulesApiResponse](url)
   }
 }
